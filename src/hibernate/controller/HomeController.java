bf8b56385fba9ab0c734a3ba3cbc3ae2495259e9
Carter <cthostenson2@my.wctc.edu>
2019-12-04 17:46:15 -0600

    Another Try

--- src/hibernate/controller/HomeController.java
+++ src/hibernate/controller/HomeController.java
@@ -3,18 +3,19 @@ package hibernate.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/")
-    public String homePage() {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
         return "home";
     }
 
-    @RequestMapping("/error")
+    @RequestMapping(value="/error")
     public String showErrorPage(HttpServletRequest request,
                                 Model theModel) {
         int httpErrorCode = (Integer)request.getAttribute("javax.servlet.error.status_code");
